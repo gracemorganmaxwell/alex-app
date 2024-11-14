badf166470a96a7c8d37ad1075bb41f23c9247d4
grace-morganmaxwell <gracemorganmaxwell@gmail.com>
2024-11-14 14:41:20 +1300

    added home and about pages and layout

---
+++ web/src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,15 @@
+import { Metadata } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <Metadata title="About" description="About page" />
+      <p>
+        This site was created to demonstrate how to build a web app with
+        RedwoodJS.
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
